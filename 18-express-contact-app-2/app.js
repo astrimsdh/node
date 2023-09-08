bf2e01734299165f20ba7dd4e63bfae031312d1e
Astri Musidah <astry.musidah@gmail.com>
2023-09-08 23:47:24 +0700

    Menyelesaikan video ke 18, dimana membuat contact app dibagian 2, disini ditambahkan fitur penambahan data, validasi apabila inputan tidak sesuai dengan menggunakan package express validator. Kemudian, menambahkan flash apabila data berhasil ditambahkan, disini menggunakan 3 package. Session, cookieParser,dan flash

--- 18-express-contact-app-2/app.js
+++ 18-express-contact-app-2/app.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
+const { loadContact, findContact, addContact, cekDuplikat } = require('./utils/contacts');
+const { body, validationResult, check } = require('express-validator');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
+
 const app = express()
 const port = 3000
-const { loadContact, findContact, addContact } = require('./utils/contacts');
 
 // gunakan ejs
 app.set('view engine', 'ejs');
 app.use(expressLayout);
 app.use(express.static('public')); // Built-in Middleware
 app.use(express.urlencoded({extended: true}));
+
+
+// Konfigurasi Flash
+app.use(cookieParser('secret'));
+app.use(
+  session({
+    cookie: {maxAge:6000},
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 app.get('/', (req, res) => {
   const mahasiswa = [
@@ -35,13 +54,37 @@ app.get('/contact', (req, res) => {
     title: 'Halaman Contact',
     layout: 'layouts/main-layouts',
     contacts,
+    msg: req.flash('msg'),
   });
 })
 
 // Proses data contact
-app.post('/contact', (req, res) => {
-  addContact(req.body);
-  res.redirect('/contact');
+app.post('/contact', [
+  body('nama').custom((value) => {
+    const duplikat = cekDuplikat(value);
+    if(duplikat) {
+      throw new Error('Nama contact sudah digunakan!');
+    }
+    return true;
+  }),
+  check('email', 'Email tidak Valid!').isEmail(),
+  check('nohp', 'No HP tidak Valid!').isMobilePhone('id-ID')
+], (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    // return res.status(400).json({ errors: errors.array});
+    res.render('add-contact', {
+      title: 'Form Tambah Data Contact',
+      layout: 'layouts/main-layouts',
+      errors: errors.array()
+    })
+  }else{
+    addContact(req.body);
+    // kirimkan flash message
+    req.flash('msg', 'Data contact berhasil ditambahkan!');
+    res.redirect('/contact');
+  }
+ 
 })
 
 // Halaman form tambah data contact
@@ -64,6 +107,7 @@ app.get('/contact/:nama', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about', {
+    title: 'Tentang Programmer',
     layout: 'layouts/main-layouts'
   });
 })
